925e3dbb750844d76b3fd8bdad629655bc02a285
Ilja <I.Knis@campus.lmu.de>
2021-05-18 21:52:12 +0200

    Added class Robot.java

---
+++ src/main/java/game/Robot.java
@@ -0,0 +1,41 @@
+package game;
+
+/**
+ * @author Ilja Knis
+ */
+public class Robot {
+
+    private int xPosition;
+    private int yPosition;
+    private String name;
+
+    public Robot(String name, int xPosition, int yPosition){
+        this.name = name;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+    }
+
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setxPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public void setyPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
