1acc7447c82a61ecb063df0cc836887d3a647041
Viktoryia Patapovich <patapovich@cip.ifi.lmu.de>
2021-05-21 14:23:05 +0200

    ClientApplication Done.

---
+++ src/client/ClientApplication.java
@@ -0,0 +1,53 @@
+package client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+import java.util.logging.Logger;
+
+/**
+ * Class client.ClientApplication to start a client.
+ *
+ * @author Vorprojekt
+ */
+public class ClientApplication extends Application {
+
+    private static final Logger logger = Logger.getLogger(ClientApplication.class.getName());
+
+    /**
+     * starts before the method start - Lifecycle of JavaFX
+     */
+    @Override
+    public void init () {
+        logger.info("Init!");
+    }
+
+    /**
+     * starts before the application will close - Lifecycle of JavaFX
+     */
+    @Override
+    public void stop () {
+        logger.info("Stage is closing..");
+        System.exit(0);
+    }
+
+    /**
+     * Design of the Stage including the set of the scene with fxml-File and CSS-File
+     *
+     * @param stage the window of the application
+     */
+    @Override
+    public void start (Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/Chat.fxml")));
+        stage.setTitle("RoboRally Chat");
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/view/Chat.fxml")).toString());
+        stage.setScene(scene);
+        stage.show();
+        logger.info("Start!");
+    }
+}
