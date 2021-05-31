bafd057de5044ec78f17635f4537a1c9387b1663
Viktoryia Patapovich <patapovich@cip.ifi.lmu.de>
2021-05-31 11:01:06 +0200

    pair programming with Mohamad

--- src/main/java/json/MessageHandler.java
+++ src/main/java/json/MessageHandler.java
@@ -24,7 +24,6 @@ public class MessageHandler {
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_GREEN = "\u001B[0m";
-    private Server server = Server.getInstance();
 
 
     /**
