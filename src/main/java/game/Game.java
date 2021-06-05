d5a759dcec2479eee9419e39bafc5556567b87ec
Ilja <I.Knis@campus.lmu.de>
2021-06-05 11:02:22 +0200

    Implemented method getLaserPath

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -228,6 +228,75 @@ public class Game {
         }
     }
 
+    //TODO: expand method for laser hits robot
+    //      check for coordination consistency
+    //      check for possible exception handling
+    public ArrayList<Point2D> getLaserPath(Laser laser, Point2D laserPosition){
+        ArrayList<Point2D> laserPath = new ArrayList<>();
+        laserPath.add(laserPosition);
+        boolean foundBlocker = false;
+        double tempPosition;
+        switch(laser.getOrientations().get(0)){
+            case "top" -> {
+                while(!foundBlocker){
+                    tempPosition = laserPosition.getY();
+                    tempPosition--;
+                    laserPath.add(new Point2D(tempPosition, laserPosition.getY()));
+                    for(int i = 0; i < map.get((int) tempPosition).get((int) laserPosition.getY()).size(); i++) {
+                        if (map.get((int) tempPosition).get((int) laserPosition.getY()).get(i).getType().equals("Wall")) {
+                            foundBlocker = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            case "bottom" -> {
+                while(!foundBlocker){
+                    tempPosition = laserPosition.getY();
+                    tempPosition++;
+                    laserPath.add(new Point2D(tempPosition, laserPosition.getY()));
+                    for(int i = 0; i < map.get((int) tempPosition).get((int) laserPosition.getY()).size(); i++) {
+                        if (map.get((int) tempPosition).get((int) laserPosition.getY()).get(i).getType().equals("Wall")) {
+                            foundBlocker = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            case "left" -> {
+                while(!foundBlocker){
+                    tempPosition = laserPosition.getX();
+                    tempPosition--;
+                    laserPath.add(new Point2D(tempPosition, laserPosition.getY()));
+                    for(int i = 0; i < map.get((int) laserPosition.getY()).get((int) tempPosition).size(); i++) {
+                        if (map.get((int) laserPosition.getY()).get((int) tempPosition).get(i).getType().equals("Wall")) {
+                            foundBlocker = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            case "right" -> {
+                while(!foundBlocker){
+                    tempPosition = laserPosition.getX();
+                    tempPosition++;
+                    laserPath.add(new Point2D(tempPosition, laserPosition.getY()));
+                    for(int i = 0; i < map.get((int) laserPosition.getY()).get((int) tempPosition).size(); i++) {
+                        if (map.get((int) laserPosition.getY()).get((int) tempPosition).get(i).getType().equals("Wall")) {
+                            foundBlocker = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            default -> {
+                //Place for exception handling
+            }
+        }
+
+        return laserPath;
+    }
+
 
     //TODO if robot moves outside the map -> check map for RestartPoint
     //     -> spawn robot at RestartPoint
