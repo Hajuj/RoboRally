aa871cc454289bb8af31dcfff173e36970cb2fdf
Viktoryia Patapovich <patapovich@cip.ifi.lmu.de>
2021-06-12 04:26:30 +0200

    My first Baby AI! SimpleAI hat ein SimpleAIModel und ein MessageHandler. In dem Model werden alle wichtigen Daten und AI-Routinen-Methoden gespeichert. Aktueller Stand: AI-Baby verbindet sich mit dem Server (HelloClient-HelloServer-Welcome-Alive) und begrüßt alle im Chat (SendChat-ReceiveChat). AI-Baby wählt auch die PlayerValues: username ist immer "SimpleAI_id" und figure - die erste available Figure. Wenn alle Figuren schon besetzt sind, geht das AI-Baby von dem Server weg. AI-Baby ist immer ready (SetStatus). Wenn das Spiel beginnt, wartet das Baby bis es dran ist und dann wählt das erste nicht-besetze StartingPoint (SetStaringPoint). Während der ProgrammierPhase tut das Baby seine ersten 5 Karten ins Register rein (SelectCard) und wartet auf die anderen. Während der Aktivierungsphase wartet das Baby bis es dran ist und dann spielt die Karte aus dem currentRegister (PlayCard). Bis jetzt funktioniert alles nur bei dem LMU-Server und nur für die erste Runde.

---
+++ src/main/java/client/KI_Zeug/MessageHandlerAI.java
@@ -0,0 +1,75 @@
+package client.KI_Zeug;
+
+import client.model.ClientModel;
+import json.protocol.*;
+
+import java.util.Map;
+
+public class MessageHandlerAI extends client.model.MessageHandler {
+    private static SimpleAIModel simpleAIModel = SimpleAIModel.getInstance();
+
+
+    @Override
+    public void handleWelcome (ClientModel clientmodel, WelcomeBody welcomeBody) {
+        super.handleWelcome(clientmodel, welcomeBody);
+        simpleAIModel.setName("SimpleAI_" + welcomeBody.getClientID());
+        clientmodel.sendUsernameAndRobot(simpleAIModel.getName(), simpleAIModel.getFigureCounter());
+    }
+
+    @Override
+    public void handlePlayerAdded (ClientModel clientModel, PlayerAddedBody playerAddedBody) {
+        super.handlePlayerAdded(clientModel, playerAddedBody);
+        if (simpleAIModel.isHasPlayerValues()) {
+            clientModel.sendMsg("Hi " + playerAddedBody.getName() + "! I'm Simple AI and I love you!");
+        } else if (playerAddedBody.getClientID() == clientModel.getClientGameModel().getPlayer().getPlayerID()) {
+            simpleAIModel.setHasPlayerValues(true);
+            //AI ist immer ready
+            clientModel.setNewStatus(true);
+            for (Map.Entry<Integer, String> entry : clientModel.getPlayersNamesMap().entrySet()) {
+                //say hi zu alle ausser dich selber
+                if (entry.getKey() != clientModel.getClientGameModel().getPlayer().getPlayerID()) {
+                    clientModel.sendMsg("Hi " + entry.getValue() + "! I'm Simple AI and I love you!");
+                }
+            }
+        }
+    }
+
+    //Wegen Alert-Fenster
+    @Override
+    public void handleError (ClientModel clientmodel, ErrorBody errorBody) {
+        System.out.println(errorBody.getError());
+        //für die Automatische Figur Auswählen
+        if (errorBody.getError().equals("Figure is already taken") && !simpleAIModel.isHasPlayerValues()) {
+            simpleAIModel.chooseRobotRoutine();
+        } else if (errorBody.getError().equals("Another robot is on this tile!")) {
+            simpleAIModel.setStartingPointCounter(simpleAIModel.getStartingPointCounter() + 1);
+            simpleAIModel.setStartingPointRoutine();
+        }
+    }
+
+    @Override
+    public void handleCurrentPlayer (ClientModel clientModel, CurrentPlayerBody currentPlayerBody) {
+        super.handleCurrentPlayer(clientModel, currentPlayerBody);
+        if (currentPlayerBody.getClientID() == clientModel.getClientGameModel().getPlayer().getPlayerID()) {
+            if (clientModel.getClientGameModel().getActualPhase() == 0) {
+                simpleAIModel.setStartingPointRoutine();
+            } else if (clientModel.getClientGameModel().getActualPhase() == 3) {
+                simpleAIModel.playCardRoutine(clientModel.getClientGameModel().getActualRegister());
+            }
+        }
+    }
+
+    @Override
+    public void handleYourCards (ClientModel clientModel, YourCardsBody yourCardsBody) {
+        super.handleYourCards(clientModel, yourCardsBody);
+        if (clientModel.getClientGameModel().getActualPhase() == 2) {
+            simpleAIModel.chooseCardsRoutine();
+        }
+    }
+
+    //wegen Timer Alert
+    @Override
+    public void handleTimerStarted (ClientModel clientModel, TimerStartedBody timerStartedBody) {
+        System.out.println("You are awesome AI Baby");
+    }
+}
