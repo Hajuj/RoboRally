40165310432747c33d33ed09ffb48572f118590b
Ilja <I.Knis@campus.lmu.de>
2021-06-12 12:10:36 +0200

    Implemented special programming cards

---
+++ src/main/java/game/programmingcards/SpamFolder.java
@@ -0,0 +1,17 @@
+package game.programmingcards;
+
+import game.Card;
+
+public class SpamFolder extends Card {
+    public SpamFolder(){
+        cardName = "SpamFolder";
+    }
+
+    public void activateCard() {
+        //TODO implement card effect
+    }
+
+    public boolean isDamageCard() {
+        return false;
+    }
+}
